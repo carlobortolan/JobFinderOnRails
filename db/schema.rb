79d23770deac68bba94530de699b1cbac0926055
Carlo Bortolan <carlo.bortolan@tum.de>
2022-12-20 17:35:42 +0100

    Fixed bug for application.response

--- db/schema.rb
+++ db/schema.rb
@@ -32,25 +32,25 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_20_142335) do
   end
 
   create_table "jobs", primary_key: "job_id", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.column "job_type", "enum('typa','typb','typc')", null: true
-    t.integer "job_status", limit: 1, default: 0, null: true
-    t.string "status", default: 0, null: true
-    t.timestamp "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: true
-    t.timestamp "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: true
-    t.integer "account_id", default: 0, null: true
-    t.integer "location_id", default: 0, null: true
-    t.integer "duration", default: 0, null: true
-    t.string "code_lang",limit: 2, null: true
-    t.string "title", limit: 45, null: true
-    t.string "description", limit: 500, null: true
-    t.integer "salary", null: true
-    t.column "currency", "enum('eur','usd','chf','gbp')", null: true
+    t.column "job_type", "enum('typa','typb','typc')"
+    t.integer "job_status", limit: 1, default: 0
+    t.string "status", default: "0"
+    t.timestamp "created_at", default: -> { "CURRENT_TIMESTAMP" }
+    t.timestamp "updated_at", default: -> { "CURRENT_TIMESTAMP" }
+    t.integer "account_id", default: 0
+    t.integer "location_id", default: 0
+    t.integer "duration", default: 0
+    t.string "code_lang", limit: 2
+    t.string "title", limit: 45
+    t.string "description", limit: 500
+    t.integer "salary"
+    t.column "currency", "enum('eur','usd','chf','gbp')"
     t.string "image_url", limit: 500
-    t.datetime "start_slot", precision: nil, null: true
-    t.string "time_zone", limit: 45, null: true
-    t.integer "application_count", default: 0, null: true
-    t.integer "view_count", default: 0, null: true
-    t.integer "favorite_count", default: 0, null: true
+    t.datetime "start_slot", precision: nil
+    t.string "time_zone", limit: 45
+    t.integer "application_count", default: 0
+    t.integer "view_count", default: 0
+    t.integer "favorite_count", default: 0
     t.index ["account_id"], name: "job_information_account_id_idx"
     t.index ["location_id"], name: "job_information_location_id_idx"
   end
