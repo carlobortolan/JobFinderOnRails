88dbedef7fbf649aa62d06a8951dcd139e7fbdcb
github4touchdouble <touchedouble@gmx.de>
2023-01-18 21:36:22 +0100

    bugfixing

--- app/controllers/api/v0/authentications_controller.rb
+++ app/controllers/api/v0/authentications_controller.rb
@@ -3,17 +3,20 @@ module Api
     class AuthenticationsController < ApplicationController
       # Controller for refresh token
       protect_from_forgery with: :null_session
+
       def create
         # Todo: Implement Error handling for errors dropped while call
         # Todo: Update Doc
         # Todo: An Registraton verify anbinden (abhängig von user_type scope festlegen etc.)
         # Todo: implement refreshtoken & accesstoken approach
 
-        if user.activity_status == 1 && user.authenticate(token_params["password"]) && !UserBlacklist.find_by(user_id: user.id).present?
+        if user.activity_status == 1 && user.authenticate(token_params["password"]) && !(UserBlacklist.find_by(user_id: user.id).present?)
+          puts true
           token = AuthenticationTokenService::Refresh.call(user.id)
-          if AuthenticationTokenService::Refresh.checksum?(token) && !AuthBlacklist.find_by(token: token).present?
+          if token != false && !(AuthBlacklist.find_by(token: token).present?)
             render status: 200, json: { "token": token }
           else
+            puts false
             render status: 500, json: { "error": "Please try again later. If this error persists, we recommend to contact our support team." }
           end
 
@@ -37,13 +40,39 @@ module Api
       end
 
       def verify
-        ##
+        puts "TOKEN"
+        p token
+        if !token["token"].nil?
+          # scope ausgeben
+          sub_id = token["token"][0]["sub"]
+          sub = User.find_by(id: sub_id)
+          if sub_id.present? && sub.present?
+            render status: 200, json: { "token": { "user": sub.id } }
+          else
+            render status: 500, json: { "error": "Please try again later. If this error persists, we recommend to contact our support team." }
+          end
+        elsif !token["error"].nil?
+          render status: token["error"]["status"].to_i, json: token["error"]["errors"]
+        else
+          render status: 500, json: { "system": [
+            {
+              "error": "ERR_CRAZY",
+              "description": "Note exactly what you did before this error occurred and contact our support team."
+            }
+          ]
+          }
+        end
       end
 
       private
 
       def user
+        # enables to not explicitly define user by just calling this method
         @user ||= User.find_by(email: token_params["email"])
+      end
+
+      def token
+        @token ||= AuthenticationTokenService::Refresh.content(verify_params)
       end
 
       def token_params
@@ -57,7 +86,6 @@ module Api
     end
 
   end
-
 end
 
 
--- app/service/authentication_token_service.rb
+++ app/service/authentication_token_service.rb
@@ -1,23 +1,41 @@
 class AuthenticationTokenService
-  #Todo: make code decoding also a generic method, and then pull checksum? also up here to use the generic decoding
-  def checksum(content)
+  def self.checksum(content)
     # creates checksum based of the contents of an input array. checksum is used to manually verify that token is transmitted completely
     bin = ""
-    content.each do |v,k|
+    content.each do |v, k|
       bin = bin + k.to_s
     end
     Digest::SHA2.hexdigest(bin)
   end
 
-  def call (secret, algorithm, issuer, payload)
+  def self.checksum?(decoded_token)
+    # checks whether given checksum in a refresh token is correct. if so checksum? is true, else it is false
+    checksum = decoded_token[0]["checksum"]
+    bin = {}
+    decoded_token[0].each do |k, v|
+      if k != "checksum"
+        bin[k] = v
+      end
+    end
+    testsum = checksum(bin)
+    if testsum == checksum
+      return true
+    else
+      raise AuthenticationTokenService::InvalidChecksum
+    end
+  end
+
+  def self.call (secret, algorithm, issuer, payload)
     # creates a generic jwt
     payload["iss"] = issuer.to_s
     payload["checksum"] = checksum(payload)
     return JWT.encode payload, secret, algorithm
   end
 
+  # no generic encoding because of custom validations
+
   class Refresh
-    HMAC_SECRET = 'my$ecretK3y'
+    HMAC_SECRET = 'yTcW3y9&t<=2cYn=Qt*nYyj!+aFv^LMw&o`@'
     ALGORITHM_TYPE = 'HS256'
     ISSUER = Socket.gethostname
 
@@ -29,13 +47,16 @@ class AuthenticationTokenService
         exp = Time.now.to_i + 4 * 3600
         jti = jti(iat, ISSUER.to_s)
         if sub.present? && exp.present? && jti.present? && iat.present?
+          puts "present"
           payload = { sub: sub, exp: exp, jti: jti, iat: iat }
-          return super.call(HMAC_SECRET, ALGORITHM_TYPE, ISSUER, payload)
+          return AuthenticationTokenService.call(HMAC_SECRET, ALGORITHM_TYPE, ISSUER, payload)
         else
+          puts "not present"
           return false
         end
-      rescue
-        return false
+        # rescue
+        # puts "desaster"
+        # return false
       end
     end
 
@@ -45,55 +66,68 @@ class AuthenticationTokenService
       bin = []
       begin
         decoded_token = JWT.decode(token, HMAC_SECRET, true, { verify_jti: proc { |jti| jti?(jti) }, iss: ISSUER, verify_iss: true, verify_iat: true, required_claims: ['iss', 'sub', 'exp', 'jti', 'checksum', 'iat'], algorithm: ALGORITHM_TYPE })
+        chsm = AuthenticationTokenService.checksum?(decoded_token)
       rescue JWT::ExpiredSignature
+        puts "expired signature"
         bin.push({
                    "error": "ERR_EXPIRED",
                    "description": "Attribute has expired"
                  })
       rescue JWT::InvalidIssuerError
+        puts "Invalid issuer"
         bin.push({
                    "error": "ERR_INVALID",
                    "description": "Attribute is malformed or unknown"
                  })
       rescue JWT::InvalidJtiError
+        puts "invalid jti"
         bin.push({
                    "error": "ERR_INVALID",
                    "description": "Attribute is malformed or unknown"
                  })
       rescue JWT::InvalidIatError
+        put s"invalid iat"
         bin.push({
                    "error": "ERR_INVALID",
                    "description": "Attribute is malformed or unknown"
                  })
       rescue JWT::MissingRequiredClaim
+        puts "required claim is missing"
         bin.push({
                    "error": "ERR_BLANK",
                    "description": "A required Attribute is missing"
                  })
       rescue JWT::DecodeError
+        puts "error while decoding"
         bin.push({
-                   "error": "ERR_INVALID",
-                   "description": "Attribute is malformed or unknown"
+                   "error"=> "ERR_INVALID",
+                   "description"=> "Attribute is malformed or unknown"
                  })
-      rescue
+      rescue AuthenticationTokenService::InvalidChecksum
+        puts "checksum mismatch"
         bin.push({
-                   "error": "ERR_SERVER",
-                   "description": "Please try again later. If this error persists, we recommend to contact our support team."
+                   "error"=> "ERR_INVALID",
+                   "description"=> "Attribute is malformed or unknown"
                  })
+        #rescue
+        #bin.push({
+        #          "error" => "ERR_SERVER",
+        #          "description" => "Please try again later. If this error persists, we recommend to contact our support team."
+        #        })
       end
       if bin.empty?
-        return decoded_token
+        return { "token" => decoded_token }
       else
         errors = bin.uniq { |e| e.first }
         err500 = false
         err401 = false
         err400 = false
         errors.each do |e|
-          if e == "ERR_SERVER"
+          if e.to_s == "ERR_SERVER"
             err500 = true
-          elsif e == "ERR_BLANK"
+          elsif e.to_s == "ERR_BLANK"
             err400 = true
-          elsif e == "ERR_EXPIRED" || "ERR_INVALID"
+          elsif e.to_s == "ERR_EXPIRED" || "ERR_INVALID"
             err401 = true
           end
           if err500 || (!err400 && !err401 && !err500)
@@ -103,60 +137,57 @@ class AuthenticationTokenService
           elsif !err500 && !err400 && err401
             status = 401
           end
-          return { "status": status, "token": errors }
+          return { "error" => { "status" => status, "errors" => {"token" => errors} } }
         end
       end
+    end
 
-      def self.forbidden?(token)
-        # if token is forbidden forbidden? is true if token is allowed forbidden? is false
-        jti = content(token)
-        if !jti[0].nil? # if .content returns { "status": status, "token": errors } == nil, because {...}[0] == nil
-          return !(jti?(jti[0]["jti"]))
-        else
-          return jti # error message from content
+    def self.forbidden?(token)
+      # if token is forbidden forbidden? is true if token is allowed forbidden? is false
+      jti = content(token)
+      if !jti[0].nil? # if .content returns { "status": status, "token": errors } == nil, because {...}[0] == nil
+        return !(jti?(jti[0]["jti"]))
+      else
+        return jti # error message from content
+      end
+    end
+
+    def self.jti(iat, iss)
+      # creates a unique token identifier (made for application in refresh tokens)
+      Digest::MD5.hexdigest([iat.to_s, iss, HMAC_SECRET].join(':').to_s)
+    end
+
+    def self.jti?(jti)
+      # checks whether a specifc (refresh) token is blacklisted (via its identifier "jti")
+
+      if AuthBlacklist.find_by(token: jti).present?
+        false # user is blacklisted
+      else
+        true # user isn't blacklisted
+      end
+    end
+
+    def self.checksum?(token)
+      # checks whether given checksum in a refresh token is correct. if so checksum? is true, else it is false
+      token = content(token)[0]
+      checksum = token["checksum"]
+      bin = []
+      token.each do |k, v|
+        if k != "checksum"
+          bin.push(v)
         end
-
-        private
-
-        def self.jti(iat, iss)
-          # creates a unique token identifier (made for application in refresh tokens)
-          Digest::MD5.hexdigest([iat.to_s, iss, HMAC_SECRET].join(':').to_s)
-        end
-
-        def self.jti?(jti)
-          # checks whether a apecifc (refresh) token is blacklisted
-          # if token identifier is found in blacklist .jti? is false (so token is blocked);
-          # if token identifier isnt blacklisted .jti? is true
-          if AuthBlacklist.find_by(token: jti).present?
-            false
-          else
-            true
-          end
-        end
-
-        def self.checksum?(token)
-          # checks whether given checksum in a refresh token is correct. if so checksum? is true, else it is false
-          token = content(token)[0]
-          checksum = token["checksum"]
-          bin = []
-          token.each do |k, v|
-            if k != "checksum"
-              bin.push(v)
-            end
-          end
-          testsum = checksum(bin)
-          if testsum == checksum
-            true
-          else
-            false
-          end
-        end
+      end
+      testsum = checksum(bin)
+      if testsum == checksum
+        true
+      else
+        false
       end
     end
   end
 
   class Access
-    HMAC_SECRET = 'my$ecretK3y'
+    HMAC_SECRET = 'e&iZY9=k!D'
     ALGORITHM_TYPE = 'HS256'
     ISSUER = Socket.gethostname
 
@@ -169,17 +200,19 @@ class AuthenticationTokenService
           exp = Time.now.to_i + 300
           if sub.present? && exp.present?
             payload = { sub: sub, exp: exp }
-            return {"status": 200, "access": super.call(HMAC_SECRET, ALGORITHM_TYPE, ISSUER, payload)}
+            return { "status": 200, "access": AuthenticationTokenService.call(HMAC_SECRET, ALGORITHM_TYPE, ISSUER, payload) }
           else
-            return {"status": 500, "token": token}
+            return { "status": 500, "token": token }
           end
         rescue
-          return {"status": 500, "token": token}
+          return { "status": 500, "token": token }
         end
       else
-        return {"status": 403, "token": token}
+        return { "status": 403, "token": token }
       end
     end
   end
 
+  class InvalidChecksum < StandardError
+  end
 end
