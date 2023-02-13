15ed6ed844c0e84a50f46ce074f3f9b9dc9843e3
github4touchdouble <touchedouble@gmx.de>
2023-02-13 11:50:55 +0100

    added redundancy to error controller

--- app/controllers/api/v0/registrations_controller.rb
+++ app/controllers/api/v0/registrations_controller.rb
@@ -34,6 +34,7 @@ module Api
       end
 
       def verify
+        # verifies that an newly created account was created correctly. if so it issues an initial refresh token ()
 
         if params[:email].nil? && !params[:password].nil?
           render status: 400, json: { "email": [
@@ -89,16 +90,22 @@ module Api
                 ]
                 }
               else
-                user.update(activity_status: 1)
-                if !user.errors.empty?
-                  raise
-                else
+                if user.activity_status == 0
+                  user.update(activity_status: 1)
                   begin
                     token = AuthenticationTokenService::Refresh::Encoder.call(user.id)
                     render status: 200, json: { "refresh_token": token }
                   rescue # because the code above checked all attributes, there should not be any exceptions. if there are something strange happened (or a bug)
                     render status: 500, json: { "error": "Something went wrong while issuing your initial refresh token. Please try again later. If this error persists, we recommend to contact our support team." }
                   end
+                else # isis user already listed as active?
+                  render status: 403, json: { "account": [
+                    {
+                      "error": "ERR_UNNECESSARY",
+                      "description": "Attribute is already verified."
+                    }
+                  ]
+                  }
                 end
 
               end
